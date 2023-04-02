9793d3abe39b6bfed0a67dadf1013160af0a3517
vikalp1999 <shandilyavikalp@gmail.com>
2023-04-02 19:37:34 +0530

    signup and login ui

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import {
+    Box,
+    Button,
+    Checkbox,
+    Container,
+    Divider,
+    FormControl,
+    FormLabel,
+    Heading,
+    HStack,
+    Input,
+    Stack,
+    Text,
+    Image
+  } from '@chakra-ui/react';
+import Signup from '../component/Signup';
+
+const Login = () => {
+    const [Lemail,setLemail] = useState("")
+    const [Lpassword,setLpassword] = useState("");
+
+
+  return (
+    <Container
+    maxW="lg"
+    py={{ base: '12', md: '24' }}
+    px={{ base: '0', sm: '8' }}
+   
+  >
+    <Signup/>
+    <Stack spacing="8">
+      <Stack spacing="6">
+        <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
+          <Box margin="auto">
+           
+          </Box>
+          <Heading size={{ base: 'xs', md: 'sm' }}>
+            Log in to your account
+          </Heading>
+          <HStack spacing="1" justify="center">
+            <Text color="muted">Don't have an account?</Text>
+            <Box color={'blue.400'} fontSize="xl">
+              {/* <NavLink colorScheme="blue" to="/signup">
+                Sign up
+              </NavLink> */}
+            </Box>
+          </HStack>
+        </Stack>
+      </Stack>
+      <Box
+        py={{ base: '0', sm: '8' }}
+        px={{ base: '4', sm: '10' }}
+        bg={{ base: 'transparent', sm: 'bg-surface' }}
+        boxShadow={{ base: 'none', sm: 'md' }}
+        borderRadius={{ base: 'none', sm: 'xl' }}
+      >
+        <Stack spacing="6">
+          <Stack spacing="5">
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input
+                id="email"
+                type="text"
+                name="email"
+                
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="email">Password</FormLabel>
+              <Input
+                id="password"
+                type="password"
+                name="password"
+                
+              />
+            </FormControl>
+          </Stack>
+          <HStack justify="space-between">
+            <Checkbox defaultChecked>Remember me</Checkbox>
+            <Button variant="link" colorScheme="blue" size="sm">
+              Forgot password?
+            </Button>
+          </HStack>
+          <Stack spacing="6">
+            <Button
+              variant="primary"
+              
+              bgColor="blue.400"
+              color={'white'}
+            >
+              Log In
+            </Button>
+            <HStack>
+              <Divider />
+              <Text fontSize="sm" whiteSpace="nowrap" color="muted">
+                {/* or continue with <Link to="/signup">Sign In</Link> */}
+              </Text>
+              <Divider />
+            </HStack>
+          </Stack>
+        </Stack>
+      </Box>
+    </Stack>
+  </Container>
+)
+}
+
+export default Login
